0436243ff115dcdcaaf397c9c3d0c80ca3198467
inkajanssen <ijanssenuk@gmail.com>
2025-07-12 17:13:53 +0200

    collect animal data and create a new html file from template

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -3,35 +3,55 @@ import json
 def load_data(file_path):
   """ Loads a JSON file"""
 
-  with open(file_path, "r") as handle:
+  with open(file_path, "r", encoding='utf-8') as handle:
     return json.load(handle)
 
 
-def print_animal_data(data:dict):
+def load_html(file_path):
+    """Load the content of an HTML file"""
+
+    with open(file_path, "r", encoding='utf-8') as handle:
+        return handle.read()
+
+
+def save_html(file_path, content):
+    """Save the content to an HTML file"""
+
+    with open(file_path, "w", encoding='utf-8') as handle:
+        return handle.write(content)
+
+
+def collect_animal_data(animal_data:list):
     """
     Reads the content of animals_data.json, iterates through the animals,
     print data
     :return: Name, Diet, The first location from the locations list,Type
     """
 
-    if "name" in data:
-        print(f"Name: {data["name"]}")
-    if "diet" in data["characteristics"]:
-        print(f"Diet: {data["characteristics"]["diet"]}")
-    if "locations" in data:
-        print(f"Location: {data["locations"][0]}")
-    if "type" in data["characteristics"]:
-        print(f"Type: {data["characteristics"]["type"]}")
-    print("---")
+    output = ""
+    for data in animal_data:
+        if "name" in data:
+            output+= f"Name: {data["name"]}\n"
+        if "diet" in data["characteristics"]:
+            output+=f"Diet: {data["characteristics"]["diet"]}\n"
+        if "locations" in data:
+            output+=f"Location: {data["locations"][0]}\n"
+        if "type" in data["characteristics"]:
+            output+=f"Type: {data["characteristics"]["type"]}\n"
+    return output
 
 
 def main():
     """
-    Read the content of the file
+    Read the content of the json file and the html template
+    Gather some data about the animals
+    Create a new html file
     """
     animals_data = load_data('animals_data.json')
-    for data in animals_data:
-        print_animal_data(data)
+    html_content = load_html("animals_template.html")
+    output = collect_animal_data(animals_data)
+    replace_info = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
+    save_html("animals.html", replace_info)
 
 
 if __name__ == '__main__':
