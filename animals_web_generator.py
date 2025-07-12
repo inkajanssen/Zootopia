7c1806447572915aef777f010d5f691d7f0fce7a
inkajanssen <ijanssenuk@gmail.com>
2025-07-12 19:38:55 +0200

    added selection by skin type

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -21,10 +21,24 @@ def save_html(file_path, content):
         return handle.write(content)
 
 
-def collect_animal_data(data:dict):
+def get_skin_types(animals_data:list)->list:
+    """
+    Gather all skin types from json file
+    :param animals_data:
+    :return: a unique list of skin types
+    """
+
+    skin_types = []
+    for data in animals_data:
+        skin_types.append(data["characteristics"]["skin_type"])
+
+    return list(set(skin_types))
+
+
+def collect_animal_data(data:dict)->str:
     """
     Reads the content of  a single animals, collects its data
-    :return: an HTML card with the gathered information
+    :return: a string for an HTML card with the gathered information
     """
 
     output = ""
@@ -32,18 +46,19 @@ def collect_animal_data(data:dict):
     if "name" in data:
         output+= '<div class="card__title">' + f'{data["name"]}</div>\n'
     if "scientific_name" in data["taxonomy"]:
-        output+= f'<small><i> {data["taxonomy"]["scientific_name"]}</i></small><br/>\n'
+        output+= f'<dic class="card_subtitle"><i> {data["taxonomy"]["scientific_name"]}</i></div>\n'
     output += '<p class ="card__text">'
+    output += '<ul>'
     if "diet" in data["characteristics"]:
-        output+= '<strong>Diet:</strong>' + f' {data["characteristics"]["diet"]}<br/>\n'
+        output+= '<li><strong>Diet:</strong>' + f' {data["characteristics"]["diet"]}</li>\n'
     if "locations" in data:
-        output+= '<strong>Location:</strong>' + f' {data["locations"][0]}<br/>\n'
+        output+= '<li><strong>Location:</strong>' + f' {data["locations"][0]}</li>\n'
     if "type" in data["characteristics"]:
-        output+= '<strong>Type:</strong>' + f' {data["characteristics"]["type"]}<br/>\n'
-    if "slogan" in data["characteristics"]:
-        output += '<br>'
-        output += f'<cite>{data["characteristics"]["slogan"]}</cite><br/>\n'
+        output+= '<li><strong>Type:</strong>' + f' {data["characteristics"]["type"]}</li>\n'
+    output += '</ul>'
     output += '</p>'
+    if "slogan" in data["characteristics"]:
+        output += f'<div class="slogan"> <cite>{data["characteristics"]["slogan"]}</cite></div>\n'
     output += '</li>'
     return output
 
@@ -51,16 +66,29 @@ def collect_animal_data(data:dict):
 def main():
     """
     Read the content of the json file and the html template
-    Gather data about the animals
+    Gather all skin types and ask user which one they want
+    Gather data about the animals with that skin type
     Create a new html file
     """
 
     animals_data = load_data('animals_data.json')
     html_content = load_html("animals_template.html")
 
+    list_of_skin_types = get_skin_types(animals_data)
+    while True:
+        selected_skin_type = (
+            input(f"Please select a skin type of the following ones"
+                  f" {list_of_skin_types}:"))
+        if selected_skin_type in list_of_skin_types:
+            break
+        else:
+            print("Please enter a valid skin type!")
+
+
     output = ""
     for data in animals_data:
-        output += collect_animal_data(data)
+        if data["characteristics"]["skin_type"] == selected_skin_type:
+            output += collect_animal_data(data)
 
     replace_info = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
     save_html("animals.html", replace_info)
